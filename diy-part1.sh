7ee2281b213500e14f05ee5a109ddeff23d9339f
Mint Space <13216285978@163.com>
2023-01-01 14:54:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-src-get kiddin9 https://github.com/kiddin9/openwrt-packages.git
+src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git
